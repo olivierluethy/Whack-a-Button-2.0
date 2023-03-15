6cf29f7e88f1f893098144b755632e83a1ef43ab
Olivier_Luethy <olivier@kauz.ch>
2023-03-15 16:16:02 +0100

    Better formatation for JavaScript

--- script.js
+++ script.js
@@ -9,53 +9,53 @@ const viewportHeight = window.innerHeight;
 for (let i = 0; i < 100; i++) {
     // Create a div element
     const box = document.createElement('div');
-    
+
     // Add the 'box' class to the div element
     box.classList.add('box');
-    
+
     // Append the div element to the container
     container.appendChild(box);
-    
+
     // If this is the 15th box, set its background color to yellow
     if (i === 15) {
-    box.style.backgroundColor = "yellow";
+        box.style.backgroundColor = "yellow";
     }
-    
+
     // Set the box's position to a random value within the viewport
     box.style.top = Math.random() * (viewportHeight - 50) + "px";
     box.style.left = Math.random() * (viewportWidth - 50) + "px";
-    }
+}
 
 const moveYourAss = setInterval(() => {
-  elements = document.getElementsByClassName("box");
-  for (let i = 0; i < elements.length; i++) {
-    let randomDIr = Math.floor(Math.random() * 2);
-    let x = Math.floor(Math.random() * 10);
-    let y = Math.floor(Math.random() * 10);
+    elements = document.getElementsByClassName("box");
+    for (let i = 0; i < elements.length; i++) {
+        let randomDIr = Math.floor(Math.random() * 2);
+        let x = Math.floor(Math.random() * 10);
+        let y = Math.floor(Math.random() * 10);
 
-    let nextTop = parseInt(elements[i].style.top) + (randomDIr === 1 ? x : -x);
-    let nextLeft = parseInt(elements[i].style.left) + (randomDIr === 1 ? y : -y);
+        let nextTop = parseInt(elements[i].style.top) + (randomDIr === 1 ? x : -x);
+        let nextLeft = parseInt(elements[i].style.left) + (randomDIr === 1 ? y : -y);
 
-    if (nextTop >= 0 && nextTop <= viewportHeight - 50 && nextLeft >= 0 && nextLeft <= viewportWidth - 50) {
-      elements[i].style.top = nextTop + "px";
-      elements[i].style.left = nextLeft + "px";
+        if (nextTop >= 0 && nextTop <= viewportHeight - 50 && nextLeft >= 0 && nextLeft <= viewportWidth - 50) {
+            elements[i].style.top = nextTop + "px";
+            elements[i].style.left = nextLeft + "px";
+        }
     }
-  }
 }, 1);
 /* Check if clicked */
 clickedBox = document.getElementsByClassName("box");
 for (let i = 0; i < clickedBox.length; i++) {
-clickedBox[i].addEventListener("click", function() {
-console.log("Box " + i + " has been clicked!");
-if(clickedBox[i].style.backgroundColor == "yellow"){
-    console.log("You have won!");
-    fetchWinningGif();
-    modal.style.display = "block";
-    clearInterval(moveYourAss);
-}else {
-    console.log("Bad!!");
-}
-});
+    clickedBox[i].addEventListener("click", function() {
+        console.log("Box " + i + " has been clicked!");
+        if (clickedBox[i].style.backgroundColor == "yellow") {
+            console.log("You have won!");
+            fetchWinningGif();
+            modal.style.display = "block";
+            clearInterval(moveYourAss);
+        } else {
+            console.log("Bad!!");
+        }
+    });
 }
 
 
@@ -70,17 +70,17 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
-function restartIt(){
+function restartIt() {
     location.reload();
 }
 
@@ -91,7 +91,7 @@ async function fetchWinningGif() {
     const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=winning&rating=g`);
     const data = await response.json();
     const imageUrl = data.data.images.fixed_height.url;
-    
+
     // display the gif on an image
-    showGif.src=imageUrl;
-    }
+    showGif.src = imageUrl;
+}
